ab9f15e6add8af72935a90d61bf787abd8118dc9
Robin Heinemann <robin.ole.heinemann@t-online.de>
2016-08-20 01:46:42 +0200

    add todo's

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
-#include "config.h"
 #include "args.h"
+#include "config.h"
 
-// ToDo(robin): cmd auto completion
+//ToDo(robin): cleaup complete.h a bit
+//ToDo(robin): find out if the double completion bug can be fixed (hwinfo battery energy_full <TAB>)
 
 int main(int argc, char ** argv) {
+    // std::cout << "b" << from_arr<std::string>(argv, argc) << std::endl;
     parse_args(from_arr<std::string>(argv, argc));
 }
